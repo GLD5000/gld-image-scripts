9df427e3e3d83646735beb05686b7c6724974031
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-07-02 16:01:59 +0100

    Add object only script

---
+++ src/utils/file-scripts/imageProcessing/batchImageObject.mjs
@@ -0,0 +1,17 @@
+import { listFileFullPaths } from '../general/fileOperations.mjs';
+import { renameFiles } from './imageEditing.mjs';
+import {  createTimestampArrow } from '../general/timestamp.mjs';
+import { makeImageObjectsV4 } from './createImageObjectsV4.mjs';
+import { saveImageObject } from './saveImageObject.mjs';
+import path from 'path';
+
+
+console.log('starting', createTimestampArrow());
+const { fileList } = await listFileFullPaths();
+// console.log('fileList:', fileList);
+// const { newFileList } = await renameFiles(fileList);
+// console.log('newFileList:', newFileList);
+const fileObject = await makeImageObjectsV4(fileList);
+// console.log('fileObject:', fileObject);
+const pathIn = path.dirname(fileList[0]) 
+saveImageObject(fileObject,pathIn);
