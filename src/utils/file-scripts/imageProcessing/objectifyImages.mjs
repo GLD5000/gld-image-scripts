f85adf0e654e99e1c844599472aefd98383e714e
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-07-07 17:09:36 +0100

    Cleanup unused files

--- src/utils/file-scripts/imageProcessing/objectifyImages.mjs
+++
@@ -1,9 +0,0 @@
-import { listFileNames } from '../general/fileOperations.mjs';
-import { makeImageObjectsV2 } from './createImageObjectsV2.mjs';
-import { saveImageObject } from './saveImageObject.mjs';
-
-const { fileList } = await listFileNames();
-// console.log('fileList:', fileList);
-const fileObject = await makeImageObjectsV2(fileList);
-// console.log('fileObject:', fileObject);
-saveImageObject(fileObject);
