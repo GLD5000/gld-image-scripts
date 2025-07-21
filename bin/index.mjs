544da9e6d96b698db20d6c00802ac792173404cd
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-07-21 16:44:11 +0100

    Update path resolution

--- bin/index.mjs
+++ bin/index.mjs
@@ -2,4 +2,4 @@
 
 import { runSelector } from '@gld5000-cli/script-selector'
 
-await runSelector('./node_modules/@gld5000-cli/image-scripts/package.json');
+await runSelector('node_modules/@gld5000-cli/image-scripts');
